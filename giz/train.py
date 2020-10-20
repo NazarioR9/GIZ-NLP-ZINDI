7ad9ea940fe3b82a4f34492f148a93c0d11b6cd8
NazarioR9 <tuomuhamed@gmail.com>
2020-10-20 13:00:25 +0200

    fix identations

--- giz/train.py
+++ giz/train.py
@@ -18,34 +18,33 @@ def train(args):
 	model = GIZModel(args)
 
 	criterion = nn.CrossEntropyLoss()
-  	opt = AdamW(model.parameters(), lr=args.lr)
+  opt = AdamW(model.parameters(), lr=args.lr)
+  device = 'cuda' if args.cuda else 'cpu'
 
-  	device = 'cuda' if args.cuda else 'cpu'
+	model.train()
+	model.to(device)
 
-  	model.train()
-  	model.to(device)
+	pbar = tqdm(range(args.epochs), desc='Training ...')
 
-  	pbar = tqdm(range(args.epochs), desc='Training ...')
+	for epoch in pbar:
+		print(f'Epoch {i+1} : ')
 
-  	for epoch in pbar:
-  		print(f'Epoch {i+1} : ')
+		epoch_loss = 0
 
-  		epoch_loss = 0
+		for i, data in enumerate(dataloder):
+			opt.zero_grad()
 
-  		for i, data in enumerate(dataloder):
-  			opt.zero_grad()
+			data['wav'] = data['wav'].to(device)
+			data['target'] = data['target'].to(device)
 
-  			data['wav'] = data['wav'].to(device)
-  			data['target'] = data['target'].to(device)
+			output = model(data['wav'])
+			loss = criterion(output, data['target'])
 
-  			output = model(data['wav'])
-  			loss = criterion(output, data['target'])
+			epoch_loss += loss.item()
 
-  			epoch_loss += loss.item()
+			loss.backward()
+			opt.step()
 
-  			loss.backward()
-  			opt.step()
+			print(f"\rLoss : {epoch_loss/(i+1)}")
 
-  			print(f"\rLoss : {epoch_loss/(i+1)}")
-
-  	save_model(model, args)
+	save_model(model, args)
