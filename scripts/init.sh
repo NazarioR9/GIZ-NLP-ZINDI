5a534461d618e29c6bd5fcd656f779aabe8c8488
NazarioR9 <tuomuhamed@gmail.com>
2020-10-20 02:04:06 +0200

    fix bugs

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,8 @@ alias python=python3
 pip install git+https://github.com/eaedk/testing-zindi-package.git -q
 pip install glob3 tqdm librosa -q
 
+mkdir data/raw/ data/processed/ 
+
 python pyscripts/init.py -username $1 -download #Connnects the user and download the dataset from zindi
 unzip -q data/raw/audio_files.zip -d data/
 unzip -q data/raw/AdditionalUtterances.zip -d data/
