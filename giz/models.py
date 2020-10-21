476679969729662f142387c4b2b5c12dac95996c
NazarioR9 <tuomuhamed@gmail.com>
2020-10-21 12:31:47 +0200

    fix

--- giz/models.py
+++ giz/models.py
@@ -1,5 +1,7 @@
-from .utils import load_model
 import torch.nn as nn
+import torchvision.models as tvm
+from .utils import *
+
 
 class GIZModel(nn.Module):
 	def __init__(self, args):
@@ -9,3 +11,57 @@ class GIZModel(nn.Module):
 
 	def forward(self, x):
 		return self.model(x)
+
+
+class DenseBlock(nn.Module):
+	def __init__(self, fc_size, dp_rate):
+		super(DenseBlock, self).__init__()
+
+		self.linear = nn.linear(fc_size, fc_size//2)
+		self.dropout = nn.Dropout(dp_rate)
+
+		nn.init.xavier_normal_(self.linear.weight)
+
+	def forward(self, x):
+		x = self.linear(x)
+		return self.dropout(x)
+
+
+
+def load_model(args):
+	"""
+		Arguments that must be in args:
+		- base_name: root name of the model
+		- model_name : model name
+		- drop_rate: percentage of droupout
+		- n_classes : nb of output classes
+
+		Optional arguments:
+		- nb_layer : number of Dense blocks in the last layer
+	"""
+
+	model_name = args.model_name
+	base_name = args.base_name
+	drop_rate = args.drop_rate
+	n_classes = args.n_classes
+
+	fc_size = __FC__[base_name][model_name]
+	fc_name = __BASE__[base_name]
+
+	try:
+		n = args.nb_layer
+	except Exception:
+		n = 1
+
+	# layers = []
+	# for _ in range(n):
+	# 	layers += [nn.Linear(fc_size, fc_size//2), nn.Dropout(drop_rate)]
+	# 	fc_size = fc_size//2
+
+	layers = [DenseBlock(fc_size//2*(i+1), drop_rate) for i in range(n)]
+	layers += [nn.Linear(fc_size//2*n, n_classes)]
+
+	model = getattr(tvm, model_name)(pretrained=True)
+	setattr(model, fc_name, nn.Sequential(*layers))
+
+	return model
--- giz/utils.py
+++ giz/utils.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
-import torchvision.models as tvm
 
 import os
 import cv2
@@ -34,43 +33,6 @@ __FC__ = {
 		}
 }
 
-def load_model(args):
-	"""
-		Arguments that must be in args:
-		- base_name: root name of the model
-		- model_name : model name
-		- drop_rate: percentage of droupout
-		- n_classes : nb of output classes
-
-		Optional arguments:
-		- nb_layer : number of Dense blocks in the last layer
-	"""
-
-	model_name = args.model_name
-	base_name = args.base_name
-	drop_rate = args.drop_rate
-	n_classes = args.n_classes
-
-	fc_size = __FC__[base_name][model_name]
-	fc_name = __BASE__[base_name]
-
-	try:
-		n = args.nb_layer
-	except Exception:
-		n = 1
-
-	layers = []
-	for _ in range(n):
-		layers += [nn.Linear(fc_size, fc_size//2), nn.Dropout(drop_rate)]
-		fc_size = fc_size//2
-
-
-	layers += [nn.Linear(fc_size, n_classes)]
-
-	model = getattr(tvm, model_name)(pretrained=True)
-	setattr(model, fc_name, nn.Sequential(*layers))
-
-	return model
 
 
 def preprocess_mfcc(signal, sr):
